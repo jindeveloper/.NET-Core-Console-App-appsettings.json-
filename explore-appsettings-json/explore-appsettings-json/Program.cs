b8397f54679f165580f9add5b1fa2386321b52a4
Jin Vincent Necesario <39805502+jindeveloper@users.noreply.github.com>
2019-06-10 20:00:04 +0800

    adding-appsettings.json to .NET core console app

---
+++ explore-appsettings-json/explore-appsettings-json/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace explore_appsettings_json
+{
+    class Program
+    {
+        private static string connectionStrings;
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+            connectionStrings = configuration.GetConnectionString("Storage");
+ 
+            Console.WriteLine(connectionStrings);
+        }
+    }
+}
